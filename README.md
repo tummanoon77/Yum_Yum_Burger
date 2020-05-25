d36ece7f3ec07eac51781a764dc980f80976b0f7
Tummanoon Sripechpong <62575162+tummanoon77@users.noreply.github.com>
2020-05-25 17:44:55 -0400

    change on readMe and connection

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Node Express Handlebars
 # tummanoon77
 My email address:  nukgroup@yahoo.com
-My GITHUB URL : https://tummanoon77.github.io/homework07
+My GITHUB URL : https://tummanoon77.github.io/homework13
 
 ![](https://img.shields.io/github/followers/tummanoon77?style=social)
 
--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "AuraZaira77",
+  password: process.env.PASSWORD,
   database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
